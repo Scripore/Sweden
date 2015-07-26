49856dcf7692dd921ba14e8d778f9bc87d73a05b
Ming Zhang <prongzhang@gmail.com>
2015-07-26 17:41:52 -0400

    fix ActionController::InvalidAuthenticityToken error
    refer to http://stackoverflow.com/questions/941594/understand-rails-authenticity-token
    add <%= form_authenticity_token %> to view file

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,9 @@
 class Project < ActiveRecord::Base
   is_impressionable :counter_cache => true, :column_name => :views
+  validates :owner_first_name, length: { in: 2..50 }
+  validates :zipcode, length: { is: 5 }, format: {with: /\A\d{5}/}
+  validates :title, length: { in: 2..100 }
+  
 
   def title_truncated
     self.title.upcase.truncate(30)
